0328c20c1ca4405bd83e72bc1f7113248f4ab686
Salem Yaslem <s@sy.sa>
2021-09-19 10:46:00 +0300

    make the grid responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { GridLayout, GridLayoutItem } from './components/GridLayout';
+import { GridLayout, GridLayoutItem } from './components/GridLayout/GridLayout';
+import Header from './components/Header/Header';
+import "./App.css"
 
 export default () => (
-  <GridLayout columns={2}>
-    <GridLayoutItem style={{
-      backgroundColor: 'red'
-    }} column={1} columnSpan={2}>Hello 1</GridLayoutItem>
-    <GridLayoutItem style={{
-      backgroundColor: 'red'
-    }} column={1} columnSpan={2} priority={2}>Hello 2</GridLayoutItem>
-    <GridLayoutItem style={{
-      backgroundColor: 'red'
-    }} column={1} columnSpan={2} priority={1}>Hello 3</GridLayoutItem>
-  </GridLayout>
+  <>
+    <Header></Header>
+    <GridLayout columns={3} columnGap="5px" rowGap="5px">
+      <GridLayoutItem column={1}>Hello 1</GridLayoutItem>
+      <GridLayoutItem column={2} priority={2}>Hello 2</GridLayoutItem>
+      <GridLayoutItem column={3} priority={1}>Hello 3</GridLayoutItem>
+    </GridLayout>
+  </>
 );
--- src/components/GridLayout/GridLayout.tsx
+++ src/components/GridLayout/GridLayout.tsx
@@ -3,13 +3,17 @@ import './GridLayout.css';
 
 export interface GridLayoutProps {
     columns?: number,
-    gap?: CSSProperties["gap"],
+    rows?: number,
+    columnGap?: CSSProperties["columnGap"],
+    rowGap?: CSSProperties["rowGap"],
     style?: CSSProperties
 };
 
 export interface GridLayoutItemProps {
     column?: number,
     columnSpan?: number,
+    row?: number,
+    rowSpan?: number,
     priority?: number,
     style?: CSSProperties
 };
@@ -17,18 +21,22 @@ export interface GridLayoutItemProps {
 export class GridLayout extends Component<GridLayoutProps> {
     static defaultProps: GridLayoutProps = {
         columns: 1,
-        gap: 0
+        rows: NaN,
+        columnGap: 0,
+        rowGap: 0
     }
 
     render() {
         let childrensSortedByPriority = Children.toArray(this.props.children).sort((firstElement: any, secondElement: any) => {
             return (firstElement.props.priority - secondElement.props.priority) || 1;
         });
-        return <div style={{
+        return <div className="GridLayout" style={{
             ...this.props.style,
             display: 'grid',
-            gridTemplateColumns: `repeat(${this.props.columns}, 1fr)`,
-            columnGap: this.props.gap
+            gridTemplateColumns: this.props.columns ? 'auto' : `repeat(${this.props.columns}, 1fr)`,
+            gridTemplateRows: this.props.rows ? `repeat(${this.props.rows}, 1fr)` : 'auto',
+            columnGap: this.props.columnGap,
+            rowGap: this.props.rowGap,
         }}>{childrensSortedByPriority}</div>
     }
 }
@@ -36,14 +44,17 @@ export class GridLayout extends Component<GridLayoutProps> {
 export class GridLayoutItem extends Component<GridLayoutItemProps> {
     static defaultProps: GridLayoutItemProps = {
         column: 1,
-        priority: NaN,
-        columnSpan: 0
+        columnSpan: 0,
+        row: NaN,
+        rowSpan: 0,
+        priority: NaN
     }
 
     render() {
-        return <div style={{
+        return <div className="GridLayoutItem" style={{
             ...this.props.style,
-            gridColumn: `${this.props.column}/${this.props.column! + this.props.columnSpan!}`
+            gridColumn: this.props.column ? `${this.props.column}/${this.props.column! + this.props.columnSpan!}` : 'auto',
+            gridRow: this.props.row ? `${this.props.row}/${this.props.row! + this.props.rowSpan!}` : 'auto'
         }}>{this.props.children}</div>
     }
 }
